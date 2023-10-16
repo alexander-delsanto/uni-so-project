f02573125ba6f397e04e97d92d5212eab9ead2d7
TeaBit76 <alexander.delsanto@edu.unito.it>
2023-10-16 13:43:53 +0200

    first implementation of read_constants_from_file

--- src/master.c
+++ src/master.c
@@ -6,16 +6,24 @@
 #include <unistd.h>
 #include <string.h>
 
+#define NUM_CONST 16
+
 pid_t master_pid;
-pid_t *children_pid;
-int children_num;
+pid_t *children_pid = NULL;
+int children_num = 0;
+
+struct data read_constants_from_file(char *path);
+void signal_handler(int signal);
+void close_all();
 
 int main()
 {
 	struct sigaction sa;
 	sigset_t masked_signals;
+    struct data read_data;
+    int i = 0;
 
-	master_pid = getpid();
+    master_pid = getpid();
 
 	/* Signal handler initialization */
 	bzero(&sa, sizeof(sa));
@@ -26,25 +34,53 @@ int main()
 	sigaction(SIGTERM, &sa, NULL);
 	sigaction(SIGINT, &sa, NULL);
 
+    read_data = read_constants_from_file("../constants.txt");
+    dprintf(1, "%lf\n", read_data.so_lato);
+    for(i = 0; i < NUM_CONST - 1; i++) {
+        dprintf(1, "%d\n", (&read_data.so_days)[i]);
+    }
 
 
 }
 
-void read_constants_from_file(char *path)
+struct data read_constants_from_file(char *path)
 {
 	FILE *file;
-	int shm_id;
-	struct data *read_data;
-	shm_id = get_shm(SHM_DATA_KEY, sizeof(*read_data));
-	attach_shm(shm_id, 0);
+    char c;
+    int n_char, counter = 0;
+    double value;
+	struct data read_data;
 
 	file = fopen(path, "r");
 	if(file == NULL)
 		close_all();
+    while((n_char = fscanf(file, "%lf", &value)) != EOF){
+        if(n_char != 0) {
+            if(counter >= NUM_CONST) {
+                fclose(file);
+                close_all();
+            }if (value <= 0) {
+                fclose(file);
+                close_all();
+            }
+            if (counter <= 0) {
+                read_data.so_lato = value;
+            } else {
+                (&read_data.so_days)[counter - 1] = (int) value;
+            }
+            counter++;
+        }
 
-
-
+        fscanf(file, "%*[ \t]");
+        if((c = fgetc(file)) == '#') {
+            fscanf(file, "%*[^\n]");
+        }else if(!(c >= '0' && c <= '9')) {
+            fclose(file);
+            close_all();
+        }
+    }
 	fclose(file);
+    return read_data;
 }
 
 void signal_handler(int signal)
