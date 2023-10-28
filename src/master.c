93af34a9ee9e984611300100dcb514dbc5a71282
TeaBit76 <alexander.delsanto@edu.unito.it>
2023-10-28 11:43:48 +0200

    adjusted indentation

--- src/master.c
+++ src/master.c
@@ -20,10 +20,10 @@ int main()
 {
 	struct sigaction sa;
 	sigset_t masked_signals;
-    struct data read_data;
-    int i = 0;
+	data_general read_data;
+	int i = 0;
 
-    master_pid = getpid();
+	master_pid = getpid();
 
 	/* Signal handler initialization */
 	bzero(&sa, sizeof(sa));
@@ -34,11 +34,8 @@ int main()
 	sigaction(SIGTERM, &sa, NULL);
 	sigaction(SIGINT, &sa, NULL);
 
-    read_data = read_constants_from_file("../constants.txt");
-    dprintf(1, "%lf\n", read_data.so_lato);
-    for(i = 0; i < NUM_CONST - 1; i++) {
-        dprintf(1, "%d\n", (&read_data.so_days)[i]);
-    }
+	read_data = read_constants_from_file("../constants.txt");
+	initialize_general_shm(&read_data);
 
 
 }
@@ -46,41 +43,41 @@ int main()
 data_general read_constants_from_file(char *path)
 {
 	FILE *file;
-    char c;
-    int n_char, counter = 0;
-    double value;
-	struct data read_data;
+	char c;
+	int n_char, counter = 0;
+	double value;
+	data_general read_data;
 
 	file = fopen(path, "r");
 	if(file == NULL)
 		close_all();
-    while((n_char = fscanf(file, "%lf", &value)) != EOF){
-        if(n_char != 0) {
-            if(counter >= NUM_CONST) {
-                fclose(file);
-                close_all();
-            }if (value <= 0) {
-                fclose(file);
-                close_all();
-            }
-            if (counter <= 0) {
-                read_data.so_lato = value;
-            } else {
-                (&read_data.so_days)[counter - 1] = (int) value;
-            }
-            counter++;
-        }
+	while((n_char = fscanf(file, "%lf", &value)) != EOF){
+		if(n_char != 0) {
+			if(counter >= NUM_CONST) {
+				fclose(file);
+				close_all();
+			}if (value <= 0) {
+				fclose(file);
+				close_all();
+			}
+			if (counter <= 0) {
+				read_data.so_lato = value;
+			} else {
+				(&read_data.so_days)[counter - 1] = (int) value;
+			}
+			counter++;
+		}
 
-        fscanf(file, "%*[ \t]");
-        if((c = fgetc(file)) == '#') {
-            fscanf(file, "%*[^\n]");
-        }else if(!(c >= '0' && c <= '9')) {
-            fclose(file);
-            close_all();
-        }
-    }
+		fscanf(file, "%*[ \t]");
+		if((c = fgetc(file)) == '#') {
+			fscanf(file, "%*[^\n]");
+		}else if(!(c >= '0' && c <= '9')) {
+			fclose(file);
+			close_all();
+		}
+	}
 	fclose(file);
-    return read_data;
+	return read_data;
 }
 
 void signal_handler(int signal)
