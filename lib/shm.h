b690074e1efed1995cd2659ea0e7bde3b6911f10
TeaBit76 <alexander.delsanto@edu.unito.it>
2023-11-04 17:24:48 +0100

    shm.c and shm.h overhaul

--- lib/shm.h
+++ lib/shm.h
@@ -1,20 +1,43 @@
+/**
+* @file shm.h
+* @brief Library that provides a more user friendly interface for System V shared memory.
+*/
+
 #ifndef SHM_H
 #define SHM_H
 
 #include <sys/shm.h>
 #include <sys/stat.h>
 
-typedef struct SHM shm;
-typedef unsigned int shm_id_t;
+/**
+* @brief Creates a new shared memory segment.
+*
+* @param key the key for the shared memory.
+* @param size the size of the segment.
+* @return the id of the created segment. If segment exists with key the id of the existing segment.
+*/
+int shm_create(key_t key, size_t size);
 
-shm *shm_create(key_t key, size_t length);
-void shm_delete(shm *shared);
+/**
+* @brief Deletes the shared memory segment.
+*
+* @param id_shm the id of the shared memory segment.
+*/
+void shm_delete(int id_shm);
 
-shm_id_t shm_get_id(shm *shared);
-shm *shm_attach(shm_id_t id, size_t length);
-void shm_detach(shm *shared);
+/**
+* @brief Attaches a shared memory segment.
+*
+* @param id_shm the id of the shared memory segment.
+* @return
+*/
+void *shm_attach(int id_shm);
 
-void *shm_read(shm *shared);
-void shm_write(shm *shared, const void *data, size_t length);
+/**
+* @brief Detaches a shared memory segment.
+*
+* @param shm_ptr the pointed to the segment.
+*/
+void shm_detach(void *shm_ptr);
 
 #endif
