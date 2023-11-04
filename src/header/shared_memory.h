4ddc81ab670fd22a8bff722bebbc03b7f774ca11
TeaBit76 <alexander.delsanto@edu.unito.it>
2023-11-04 18:11:36 +0100

    defining data structures for testing

--- src/header/shared_memory.h
+++ src/header/shared_memory.h
@@ -7,23 +7,39 @@
 
 #define SHM_DATA_GENERAL_KEY 0x1fff
 #define SHM_DATA_PORTS_KEY 0x2fff
+#define SHM_DATA_SHIPS_KEY 0x3fff
+#define SHM_DATA_CARGO_KEY 0x4fff
 
-typedef struct _data_general{
+
+struct data_general{
 	double so_lato;
 	int so_days;
 	int so_navi, so_speed, so_capacity;
 	int so_porti, so_banchine, so_fill, so_loadspeed;
 	int so_merci, so_size, so_min_vita, so_max_vita;
 	int so_storm_duration, so_swell_duration, so_maelstrom;
-} data_general;
+};
+
+struct coordinates{
+	double x;
+	double y;
+};
 
 struct data_port{
 	pid_t pid;
-	double x_coord;
-	double y_coord;
+	struct coordinates coord;
 };
 
+struct data_ship{
+	pid_t pid;
+	struct coordinates coord;
+};
 
+struct data_cargo{
+	int id;
+	int quantity;
+	int time_of_life;
+};
 
 
 #endif
