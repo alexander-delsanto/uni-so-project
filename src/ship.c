b71aafbad15aefa4f8784ab33c6275c6ab4a5d9b
Ela17 <derosaselena99@gmail.com>
2023-11-05 10:32:24 +0100

    Updating according to modified/new functions in shm and ipc_utils

--- src/ship.c
+++ src/ship.c
@@ -6,69 +6,49 @@
 #include <math.h>
 #include "header/shared_memory.h"
 #include "../lib/shm.h"
-
-int base = 100; // example base of the map
-//int capacity = 1000; // example capacity
-int speed = 10; // example speed
+#include "header/ipc_utils.h"
 
 struct data_ship *ship;
-struct coordinates *dest;
-
-shm *ship_shm;
-
-#define SIZE (sizeof(*ship))
+int this_shm_id;
 
 void init_location();
 /*void signal_handler(int signal);*/
 void move();
-void convert_and_sleep(double param, int speed);	/* we need it for move() and mooring in port */
-struct data_cargo * generate_cargo();
+void convert_and_sleep(double param, int speed);
 
 int main()
 {
-	//shm_id_t this_id;
+	//shm_id_t this_shm_id;
 	int cargo_quantity = 0;	/* counter for total objects in cargo
  * 					must be in [0,so_capacity] */
-	struct data_cargo *my_cargo = NULL;
-	/* init signals */
 
-	/* obtain the necessary data from data_general
-	 * (so_speed, so_capacity, so_lato)
-	 */
-	
-	/* example */
-	dest = malloc(sizeof(*dest));
-	dest->x = 6.0;
-	dest->y = 8.0;
-	ship = malloc(SIZE);
-
-	ship_shm = shm_create(SHM_DATA_SHIPS_KEY, SIZE);
+	initialize_shm();
+	this_shm_id = get_ship_shm_id();
 	
 	init_location();
+
 	
-	my_cargo = generate_cargo();
-	
-	ship = shm_read(ship_shm);
+	//my_cargo = generate_cargo();
+
 	fprintf(stdout, "\nprima: coord = (%f,%f)\n", ship->coord.x, ship->coord.y);
 	
 	/* try to move */
 	move();
-	shm_write(ship_shm, (void*) ship, SIZE);
-	ship = shm_read(ship_shm);
 	fprintf(stdout, "\ndopo: coord = (%f,%f)\n", ship->coord.x, ship->coord.y);
 
-	free(ship);
-	shm_delete(ship_shm);
-
-        return 0;
+    return 0;
 }
 
+/**
+ * @brief initializes ship's location.
+ */
 void init_location()
 {
         /* generate a random location on the map */
 	srand((unsigned int)time(NULL) * getpid());
-	ship->coord.x = rand() % base;
-	ship->coord.y = rand() % base;
+	ship->coord.x = RANDOM_DOUBLE(0, SO_LATO);
+	ship->coord.y = RANDOM_DOUBLE(0, SO_LATO);
+	set_ship_coords(this_shm_id, ship->coord);
 }
 
 void signal_handler(int signal)
@@ -76,43 +56,39 @@ void signal_handler(int signal)
          /* TODO */
 }
 
+/**
+ * @brief simulate the movement of the ship and updates the location.
+ */
 void move()
 {
         double distance;
         /* calculate distance between actual position and destination */
         distance = sqrt(pow(dest->x - ship->coord.x, 2) + pow(dest->y - ship->coord.y, 2));
-	/* conversions and wait */
-	convert_and_sleep(distance, so_speed);
+	convert_and_sleep(distance, SO_SPEED);
         /* new location */
         ship->coord = *dest;
+	set_ship_coords(this_shm_id, ship->coord);
 }
 
 /**
- *  function to decide the next port based on availability and distance ???
+ * @brief converts time data from days to nanoseconds and calls nanosleep().
+ *
+ * @param param data of the action
+ * @param speed speed required to perform the action
  */
- 
-
 void convert_and_sleep(double param, int speed)
 {
 	double time_required;
 	struct timespec sleep_time;
 	
-	/* calculate time required to terminate the operation
-         * (in days)
-         */
+	/* calculate time required to terminate the operation (in days) */
 	time_required = param / speed;
-	time_required *= SECONDS_IN_DAY; /* conversion in seconds */
+	/* conversion in seconds */
+	time_required *= SECONDS_IN_DAY;
 	
 	/* conversion in timespec to invoke nanosleep() */
 	sleep_time.tv_sec = (int) time_required;
 	sleep_time.tv_nsec = (long)(time_required - (double)sleep_time.tv_sec) * NANOSECONDS_IN_SECOND;
 	
 	nanosleep(&sleep_time, NULL);
-}
-
-struct data_cargo * generate_cargo()
-{
-	struct data_cargo *cargo = malloc(so_capacity * sizeof(*cargo));
-	
-	
 }
