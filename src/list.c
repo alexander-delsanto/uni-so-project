308553a6b2f459f29e14b6e64143b7d44cec4a0c
8lall0 <savino.liguori@gmail.com>
2023-11-22 22:57:16 +0100

    Added list with ordered insertion, expired removal and quantity popping.

---
+++ src/list.c
@@ -0,0 +1,171 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "include/list.h"
+
+struct node {
+	int quantity;
+	int expire;
+	struct node *next;
+};
+
+struct o_list {
+	struct node *head;
+};
+
+static struct node *create_node(int quantity, int expire);
+
+o_list_t *OrderedList(void)
+{
+	o_list_t *list;
+
+	list = malloc(sizeof(o_list_t));
+	if (list == NULL) {
+		return NULL;
+	}
+	list->head = NULL;
+
+	return list;
+}
+
+void add(o_list_t *list, int quantity, int expire)
+{
+	struct node *node, *prev, *cur;
+
+	if (list == NULL) {
+		return;
+	}
+
+	prev = NULL;
+	cur = list->head;
+
+	while (1) {
+		/* If list is empty or expiration date is lower than current element */
+		if (cur == NULL || cur->expire > expire) {
+			node = create_node(quantity, expire);
+			if (node == NULL) {
+				return;
+			}
+			node->next = cur;
+			if (prev != NULL) {
+				prev->next = node;
+			} else {
+				list->head = node;
+			}
+
+			break;
+		} else if (cur->expire == expire) {
+			cur->quantity += quantity;
+			break;
+		}
+		prev = cur;
+		cur = cur->next;
+	}
+}
+
+int remove_expired(o_list_t *list, int expire_day)
+{
+	struct node *tmp;
+	int qt;
+
+	if (list == NULL) {
+		return -1;
+	}
+
+	if (list->head == NULL) {
+		return 0;
+	}
+
+	qt = 0;
+
+	if (list->head->expire == expire_day) {
+		qt = list->head->quantity;
+		tmp = list->head;
+		list->head = list->head->next;
+		free(tmp);
+	}
+
+	return qt;
+}
+
+o_list_t *pop_needed(o_list_t *list, int quantity)
+{
+	o_list_t *output;
+
+	struct node *tmp;
+	int cnt;
+
+	if (list == NULL) {
+		return NULL;
+	}
+
+	if (list->head == NULL || quantity == 0) {
+		return NULL;
+	}
+
+	output = OrderedList();
+	if (output == NULL) {
+		return NULL;
+	}
+
+	cnt = quantity;
+
+	while (list->head->next != NULL || cnt > 0) {
+		if (list->head->quantity <= cnt) {
+			add(output, list->head->quantity, list->head->expire);
+			cnt -= list->head->quantity;
+			tmp = list->head;
+			list->head = list->head->next;
+			free(tmp);
+		} else {
+			add(output, cnt, list->head->expire);
+			list->head->quantity -= cnt;
+			break;
+		}
+	}
+
+	return output;
+}
+
+void delete_list(o_list_t *list)
+{
+	struct node *cur, *tmp;
+
+	cur = list->head;
+
+	while (cur != NULL) {
+		tmp = cur;
+		cur = cur->next;
+		free(tmp);
+	}
+
+	free(list);
+}
+
+void print_all(o_list_t *list)
+{
+	struct node *cur;
+
+	cur = list->head;
+
+	while (cur != NULL) {
+		printf("%d %d\n", cur->quantity, cur->expire);
+		cur = cur->next;
+	}
+}
+
+static struct node *create_node(int quantity, int expire)
+{
+	struct node *node;
+
+	node = malloc(sizeof(struct node));
+	if (node == NULL) {
+		return NULL;
+	}
+
+	node->quantity = quantity;
+	node->expire = expire;
+	node->next = NULL;
+
+	return node;
+}
