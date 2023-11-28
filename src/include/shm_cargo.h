222319c552b6d248c9d7deb2923c3f3dee25b1a4
TeaBit76 <alexander.delsanto@edu.unito.it>
2023-11-28 16:19:55 +0100

    implementing shm_cargo

---
+++ src/include/shm_cargo.h
@@ -0,0 +1,32 @@
+#ifndef OS_PROJECT_SHM_CARGO_H
+#define OS_PROJECT_SHM_CARGO_H
+
+#include <sys/types.h>
+
+#include "shm_general.h"
+#include "types.h"
+
+typedef struct shm_cargo shm_cargo_t;
+
+/* Cargo shm */
+shm_cargo_t *shm_cargo_initialize(shm_general_t *g);
+shm_cargo_t *shm_cargo_attach(shm_general_t *g);
+void shm_cargo_detach(shm_cargo_t *c);
+void cargo_shm_delete(shm_general_t *c);
+
+/* Getters */
+int shm_cargo_get_size(shm_cargo_t *c, int id);
+int shm_cargo_get_life(shm_cargo_t *c, int id);
+
+int shm_cargo_get_dump_total_generated(shm_cargo_t *c, int id);
+int shm_cargo_get_dump_stayed_in_port(shm_cargo_t *c, int id);
+int shm_cargo_get_dump_expired_in_port(shm_cargo_t *c, int id);
+int shm_cargo_get_dump_expired_on_ship(shm_cargo_t *c, int id);
+
+/* Setters */
+void shm_cargo_set_dump_total_generated(shm_cargo_t *c, int id, int quantity);
+void shm_cargo_set_dump_stayed_in_port(shm_cargo_t *c, int id, int quantity);
+void shm_cargo_set_dump_expired_in_port(shm_cargo_t *c, int id, int quantity);
+void shm_cargo_set_dump_expired_on_ship(shm_cargo_t *c, int id, int quantity);
+
+#endif
---
+++ src/shm_cargo.c
@@ -0,0 +1,117 @@
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+
+#include "../lib/shm.h"
+#include "../lib/semaphore.h"
+
+#include "include/utils.h"
+#include "include/const.h"
+#include "include/shm_general.h"
+#include "include/types.h"
+#include "include/shm_port.h"
+#include "include/shm_cargo.h"
+
+static void shm_cargo_values_init(shm_general_t *g, shm_cargo_t *cargo);
+
+struct shm_cargo {
+	int batch_size;
+	int batch_life;
+
+	/* TODO aggiungere dump cargo */
+	int dump_total_generated;
+	int dump_stayed_in_port;
+	int dump_expired_in_port;
+	int dump_expired_on_ship;
+	int dump_delivered_by_ship;
+};
+
+shm_cargo_t *shm_cargo_initialize(shm_general_t *g)
+{
+	shm_cargo_t *cargo;
+	int shm_id, n_cargo, i;
+	size_t size;
+
+	n_cargo = get_merci(g);
+
+	size = (sizeof(struct shm_cargo) * n_cargo);
+
+	shm_id = shm_create(SHM_DATA_CARGO_KEY, size);
+	if (shm_id == -1) {
+		return NULL;
+	}
+
+	cargo = shm_attach(shm_id);
+	bzero(cargo, size);
+	set_cargo_shm_id(g, shm_id);
+
+	shm_cargo_values_init(g, cargo);
+
+	for (i = 0; i < n_cargo; i++) {
+		dprintf(1, "cargo %d: size: %d, life: %d\n", i,
+			cargo[i].batch_size, cargo[i].batch_life);
+	}
+
+	return cargo;
+}
+
+static void shm_cargo_values_init(shm_general_t *g, shm_cargo_t *cargo)
+{
+	int i, n_cargo;
+
+	n_cargo = get_merci(g);
+	for (i = 0; i < n_cargo; i++) {
+		cargo[i].batch_size = RANDOM_INTEGER(1, get_size(g));
+		cargo[i].batch_life = RANDOM_INTEGER(get_min_vita(g), get_max_vita(g));
+	}
+}
+
+shm_cargo_t *shm_cargo_attach(shm_general_t *g)
+{
+	shm_cargo_t *cargo;
+
+	cargo = shm_attach(get_cargo_shm_id(g));
+	return cargo;
+}
+
+void shm_cargo_detach(shm_cargo_t *c)
+{
+	shm_detach(c);
+}
+
+void cargo_shm_delete(shm_general_t *c)
+{
+	shm_delete(get_cargo_shm_id(c));
+}
+
+
+
+/* Getters */
+int shm_cargo_get_size(shm_cargo_t *c, int id){return c[id].batch_size;}
+int shm_cargo_get_life(shm_cargo_t *c, int id){return c[id].batch_life;}
+
+int shm_cargo_get_dump_total_generated(shm_cargo_t *c, int id){return c[id].dump_total_generated;}
+int shm_cargo_get_dump_stayed_in_port(shm_cargo_t *c, int id){return c[id].dump_stayed_in_port;}
+int shm_cargo_get_dump_expired_in_port(shm_cargo_t *c, int id){return c[id].dump_expired_in_port;}
+int shm_cargo_get_dump_expired_on_ship(shm_cargo_t *c, int id){return c[id].dump_expired_on_ship;}
+
+/* Setters */
+void shm_cargo_set_dump_total_generated(shm_cargo_t *c, int id, int quantity)
+{
+	c[id].dump_total_generated += quantity;
+}
+void shm_cargo_set_dump_stayed_in_port(shm_cargo_t *c, int id, int quantity)
+{
+	c[id].dump_stayed_in_port += quantity;
+}
+void shm_cargo_set_dump_expired_in_port(shm_cargo_t *c, int id, int quantity)
+{
+	c[id].dump_expired_in_port += quantity;
+}
+void shm_cargo_set_dump_expired_on_ship(shm_cargo_t *c, int id, int quantity)
+{
+	c[id].dump_expired_on_ship += quantity;
+}
