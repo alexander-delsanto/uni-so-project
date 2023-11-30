0ee3d64dd7e76fe8f6d967c72c379f4a666f6595
TeaBit76 <alexander.delsanto@edu.unito.it>
2023-11-30 18:47:15 +0100

    implementing convert_and_sleep so that is continues after interrupt

--- src/utils.c
+++ src/utils.c
@@ -1,21 +1,30 @@
 #define _GNU_SOURCE
 
 #include <time.h>
+#include <errno.h>
 
 #include "include/utils.h"
 
+static struct timespec get_timespec(double time_required)
+{
+	struct timespec res;
+	res.tv_sec = (time_t)time_required;
+	res.tv_nsec = ((suseconds_t)((time_required - (double)res.tv_sec) *1e9));
+
+	return res;
+}
+
 void convert_and_sleep(double time_required)
 {
-	struct timespec sleep_time;
+	struct timespec sleep_time, remaining_time;
 
 	if (time_required <= 0)
 		return;
 
-	/* conversion in timespec to invoke nanosleep() */
-	sleep_time.tv_sec = (time_t)time_required;
-	sleep_time.tv_nsec =
-		(long)((time_required - (double)sleep_time.tv_sec) *
-		       1e9);
-
-	nanosleep(&sleep_time, NULL);
+	sleep_time = get_timespec(time_required);
+	do {
+		errno = EXIT_SUCCESS;
+		nanosleep(&sleep_time, &remaining_time);
+		sleep_time = remaining_time;
+	} while (errno == EINTR);
 }
